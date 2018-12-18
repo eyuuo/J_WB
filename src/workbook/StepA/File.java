8df55bbbe2131e7732f0deb86e9b4b7ad7de4749
eyuuo <eyuuo@192.168.0.16>
2018-12-18 18:33:19 +0900

    File

--- src/workbook/StepA/File.java
+++ src/workbook/StepA/File.java
@@ -9,7 +9,7 @@ public class File {
 	private long bytes;
 	
 	public File(){input();}
-	public void printAge() {
+	public void printFile() {
 		System.out.printf("입력하신 파일 용량은 \n %d 메가바이트,\n %d 킬로바이트,\n %d 바이트 입니다.\n",getMegabytes(),getKilobytes(), getByte());
 	}	
 	
