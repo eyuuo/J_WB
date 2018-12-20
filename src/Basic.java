6666243a429525a45e5a7bb037d3155e83546ab0
eyuuo <eyuuo@192.168.0.16>
2018-12-20 21:32:55 +0900

    basic

---
+++ src/Basic.java
@@ -0,0 +1,17 @@
+
+import java.util.Scanner;
+
+public class Basic {
+	public Basic() {input();}
+	
+	public void print() {}
+	
+	void input() {
+		Scanner s = new Scanner(System.in);
+		System.out.print("");
+	}
+
+	int get() {
+		return 0;}
+
+}
